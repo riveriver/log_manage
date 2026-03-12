1405c67e17b09a46377dcd5afae223ed82a8ed45
river <riveriverian@outlook.com>
2026-03-12 13:05:28 +0800

    refactor: simplify _write and _read functions for UART handling

--- printf_retarget.c
+++ printf_retarget.c
@@ -33,49 +33,74 @@ void specify_redirect_uart(UART_HandleTypeDef *huart) {
 }
 
 int _write(int fd, char* ptr, int len) {
-  
-  if (fd == STDOUT_FILENO || fd == STDERR_FILENO) {
 
-    if(printf_uart == NULL) {
-      errno = EBADF;
-      return -1;
-    }
+  if (fd != STDOUT_FILENO && fd != STDERR_FILENO) {
+    errno = EBADF;
+    return -1;
+  }
 
-    /* Check if FreeRTOS scheduler has started */
-    if(xTaskGetSchedulerState() != taskSCHEDULER_RUNNING) {
-      /* Use blocking mode during initialization (before FreeRTOS starts) */
-      HAL_StatusTypeDef hstatus;
-      hstatus = HAL_UART_Transmit(printf_uart, (uint8_t *) ptr, len, HAL_MAX_DELAY);
-      if (hstatus == HAL_OK)
-        return len;
-      else
-        return EIO;
-    }
-    
-    /* Use DMA after FreeRTOS starts */
-    while(printf_uart->gState != HAL_UART_STATE_READY);
-    uint32_t addr_aligned = (uint32_t)ptr & ~31U;
-    uint32_t len_aligned = ((uint32_t)ptr + len - addr_aligned + 31) & ~31U;
-    SCB_CleanDCache_by_Addr((uint32_t *)addr_aligned, len_aligned);
-    HAL_UART_Transmit_DMA(printf_uart, (uint8_t *)ptr, len);
-    while(printf_uart->gState != HAL_UART_STATE_READY);
+  if (printf_uart == NULL) {
+    errno = EBADF;
+    return -1;
+  }
+
+  if (ptr == NULL) {
+    errno = EINVAL;
+    return -1;
+  }
+
+  if (len < 0 || len > UINT16_MAX) {
+    errno = EINVAL;
+    return -1;
+  }
+
+  if (len == 0) {
+    return 0;
+  }
+
+  HAL_StatusTypeDef hstatus;
+  hstatus = HAL_UART_Transmit(printf_uart, (uint8_t *)ptr, (uint16_t)len, 10);
+  if (hstatus == HAL_OK) {
     return len;
   }
-  errno = EBADF;
+
+  errno = EIO;
   return -1;
 }
 
 int _read(int fd, char* ptr, int len) {
-  HAL_StatusTypeDef hstatus;
 
-  if (fd == STDIN_FILENO) {
-    hstatus = HAL_UART_Receive(printf_uart, (uint8_t *) ptr, 1, 1000);
-    if (hstatus == HAL_OK)
-      return 1;
-    else
-      return EIO;
+  if (fd != STDIN_FILENO) {
+    errno = EBADF;
+    return -1;
   }
-  errno = EBADF;
+
+  if (printf_uart == NULL) {
+    errno = EBADF;
+    return -1;
+  }
+
+  if (ptr == NULL) {
+    errno = EINVAL;
+    return -1;
+  }
+
+  if (len < 0) {
+    errno = EINVAL;
+    return -1;
+  }
+
+  if (len == 0) {
+    return 0;
+  }
+
+  HAL_StatusTypeDef hstatus;
+  hstatus = HAL_UART_Receive(printf_uart, (uint8_t *)ptr, 1, 100);
+  if (hstatus == HAL_OK) {
+    return 1;
+  }
+
+  errno = EIO;
   return -1;
 }
 
